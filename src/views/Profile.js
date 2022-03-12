17375490f7914c15fcb59d29e95ad59071f12dd8
Mary Beth Hunter <mary.beth.hunter@icloud.com>
2022-03-12 17:21:39 -0600

    pushing routes, views, nav, form

---
+++ src/views/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Profile() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <img
+        src="https://i.ibb.co/9q0DgQX/Refer-Button-Mockup.png"
+        alt="profile sample"
+      />
+    </div>
+  );
+}
